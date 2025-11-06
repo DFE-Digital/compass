67269d6de6faeddc15d6b371b348a4d182a9fa96
Andy Jones <andy.jones@digital.education.gov.uk>
2025-11-06 10:56:31 +0000

    Perf metrics

---
+++ Migrations/20251106104550_AddNotCapturedFieldsToProductMetricValue.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Compass.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotCapturedFieldsToProductMetricValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNotCaptured",
+                table: "ProductMetricValues",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NotCapturedReason",
+                table: "ProductMetricValues",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReasonForDifference",
+                table: "ProductMetricValues",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsNotCaptured",
+                table: "ProductMetricValues");
+
+            migrationBuilder.DropColumn(
+                name: "NotCapturedReason",
+                table: "ProductMetricValues");
+
+            migrationBuilder.DropColumn(
+                name: "ReasonForDifference",
+                table: "ProductMetricValues");
+        }
+    }
+}
