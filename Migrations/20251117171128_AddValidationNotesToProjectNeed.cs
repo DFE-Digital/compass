9faf3bd0952dc43f6e49e0032c9d98e7af5e6973
Andy Jones <andy.jones@digital.education.gov.uk>
2025-11-17 17:46:58 +0000

    Adding field for the evidence for validation on need.

---
+++ Migrations/20251117171128_AddValidationNotesToProjectNeed.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Compass.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValidationNotesToProjectNeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidatedAt",
+                table: "ProjectNeeds",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ValidationNotes",
+                table: "ProjectNeeds",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValidatedAt",
+                table: "ProjectNeeds");
+
+            migrationBuilder.DropColumn(
+                name: "ValidationNotes",
+                table: "ProjectNeeds");
+        }
+    }
+}
