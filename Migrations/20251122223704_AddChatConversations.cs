255e6e077d2e068413bce46b01c69f6fcbbab412
Andy Jones <andy.jones@digital.education.gov.uk>
2025-11-22 23:30:12 +0000

    fixing reporting and adding chatbot function

---
+++ Migrations/20251122223704_AddChatConversations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Compass.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChatConversations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatConversations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmail = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    UserAzureObjectId = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    Messages = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatConversations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatConversations");
+        }
+    }
+}
